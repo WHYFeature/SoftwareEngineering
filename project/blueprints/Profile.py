425948fd41b23ebaf3d18d8eb538546a0c33b3e0
WHYFeature <2396791058@qq.com>
2025-04-02 17:14:18 +0800

    个人中心初步

--- project/blueprints/Profile.py
+++ project/blueprints/Profile.py
@@ -6,6 +6,7 @@ from flask import redirect
 from flask import url_for
 
 from models import User
+from models import UserAddress
 from models import db
 from sqlalchemy import func
 
@@ -13,4 +14,29 @@ bp = Blueprint("Profile", __name__, url_prefix="/profile")
 
 @bp.route('/', methods=["GET", "POST"])
 def _profile():
-    return render_template('profile.html')
+    uid =session['uid']
+    datas = User.query.filter(UserAddress.uid == uid).all()
+
+    addresses = []
+    for data in datas:
+        address = {}
+        address['name'] = data.receiver
+        address['phone'] = data.phone
+        address['full_address']= data.address
+        addresses.append(address)
+
+    return render_template('profile.html',address = addresses)
+
+@bp.route('/update_profile', methods=["GET", "POST"])
+def update_profile():
+    if request.method == "POST":
+        username = request.form['username']
+
+    return redirect(url_for('Profile._profile'))
+
+@bp.route('/add_address', methods=["GET", "POST"])
+def add_address():
+    if request.method == "POST":
+        phone = request.form['phone']
+        receiver = request.form['name']
+        address = request.form['full_address']
